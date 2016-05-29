af8e3cef4ced0b1ef64b9e2fff88aee1d9513da7
Jon Ruttan <jonruttan@gmail.com>
2016-05-28 22:51:09 -0400

    feat(wx): add basic playability

--- pyscottfree-wx.py
+++ pyscottfree-wx.py
@@ -27,55 +27,110 @@ import os.path
 from pyscottfree import Saga
 
 __author__ = 'Jon Ruttan'
-__copyright__ = 'Copyright (C) 2010 Jon Ruttan'
+__copyright__ = 'Copyright (C) 2016 Jon Ruttan'
 __license__ = 'Distributed under the GNU software license'
 __version__ = '0.2.0'
 
 
 class WxSaga(Saga):
-    def __init__(self, frame, options=0, seed=None, name=None, file=None, greet=True):
-        self.frame = frame
+    def __init__(self, options=0, seed=None, name=None, file=None, greet=True):
+        self.wx_up = False
+        self.root = wx.App()
+        self.frame = MainWindow(self)
         Saga.__init__(self, options, seed, name, file, True)
+        self.wx_up = True
 
-    def exit(self, errno=0, str=None):
-        Saga.exit(self, errno, str)
+    # def exit(self, errno=0, str=None):
+    #     Saga.exit(self, errno, str)
 
     def clear_screen(self):
         Saga.clear_screen(self)
+        self.frame.win[1].Clear()
 
-    def output_reset(self, win=1, scroll=False):
-        Saga.output_reset(self, win, scroll)
+    # def output_reset(self, win=1, scroll=False):
+    #     Saga.output_reset(self, win, scroll)
 
     def output_write(self, string, win=1, scroll=True):
-        self.frame.control.AppendText(string)
+        self.frame.win[win].AppendText(string)
 
-    # def input_read(self, string='', win=1):
-    #     return Saga.input(self, win)
+    def output(self, string, win=1, scroll=True):
+        return Saga.output(self, string, win, False)
+
+    def input_read(self, string='', win=1):
+        string = self.frame.entry.GetValue()
+        self.frame.entry.Clear()
+        return string
 
     def input(self, string='', win=1):
-        self.frame.control.AppendText(string)
-        return Saga.input(self, string, win)
+        string = Saga.input(self, string, win)
+        self.frame.win[win].AppendText(string + '\n')
+        return string
+
+    def look(self):
+        self.frame.win[0].Clear()
+        Saga.look(self)
 
 
 class MainWindow(wx.Frame):
-    def __init__(self, options, seed):
+    def __init__(self, saga):
         super(MainWindow, self).__init__(None, size=(640, 480))
+        # self.statusbar.Hide()
         self.dirname = '.'
         self.CreateInteriorWindowComponents()
         self.CreateExteriorWindowComponents()
 
-        self.saga = WxSaga(self, options, seed)
+        self.saga = saga
 
     def OnControlKey(self, event):
-        print(chr(event.GetKeyCode()))
+        if event.GetKeyCode() == 13:
+            self.saga.game_loop(2)
+        else:
+            event.Skip(True)
 
     def CreateInteriorWindowComponents(self):
-        ''' Create "interior" window components. In this case it is just a
-            simple multiline text control. '''
-        self.control = wx.TextCtrl(
-            self, style=wx.TE_MULTILINE | wx.TE_READONLY
+        ''' Create "interior" window components. Three simple multiline text
+        controls. '''
+
+        # Frame
+        panel = wx.Panel(self, -1)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # Win0 - description - top
+        console0 = wx.TextCtrl(
+            panel,
+            style=wx.TE_MULTILINE | wx.TE_READONLY
         )
-        self.control.Bind(wx.EVT_CHAR, self.OnControlKey)
+        sizer.Add(console0, 1, wx.EXPAND | wx.ALL, 2)
+
+        # Win1 - transcript - middle
+        console = wx.TextCtrl(
+            panel,
+            style=wx.TE_MULTILINE | wx.TE_READONLY
+        )
+        sizer.Add(console, 1, wx.EXPAND | wx.ALL, 2)
+
+        # Win2 - text entry - bottom
+        sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        label = wx.StaticText(panel, label="Tell me what to do?")
+        sizer2.Add(label, 0, wx.ALIGN_LEFT | wx.ALIGN_CENTER_VERTICAL, 2)
+        entry = wx.TextCtrl(
+            panel,
+            size=wx.DLG_SZE(panel, wx.Size(80, -1))
+        )
+        sizer2.Add(entry, 1, wx.EXPAND | wx.ALL, 2)
+        sizer.Add(sizer2, 0, wx.EXPAND | wx.ALL, 2)
+
+        panel.SetSizer(sizer)
+
+        console0.SetDefaultStyle(wx.TextAttr('FOREST GREEN', wx.WHITE))
+        console.SetDefaultStyle(wx.TextAttr('MEDIUM BLUE', wx.WHITE))
+        entry.SetDefaultStyle(wx.TextAttr('MEDIUM RED', wx.WHITE))
+
+        self.win = console0, console, entry
+        self.entry = entry
+        self.entry.SetFocus()
+
+        entry.Bind(wx.EVT_CHAR, self.OnControlKey)
 
     def CreateExteriorWindowComponents(self):
         ''' Create "exterior" window components, such as menu and status
@@ -90,7 +145,7 @@ class MainWindow(wx.Frame):
             [(wx.ID_ABOUT, '&About', 'Information about this program',
                 self.OnAbout),
              (wx.ID_OPEN, '&Open', 'Open a new database', self.OnOpen),
-           # (wx.ID_RESTORE, '&Restore', 'Restore the current game', self.OnSave),
+             # (wx.ID_RESTORE, '&Restore', 'Restore the current game', self.OnSave),
              (wx.ID_SAVE, '&Save', 'Save the current game', self.OnSave),
              (None, None, None, None),
              (wx.ID_EXIT, 'E&xit', 'Terminate the program', self.OnExit)]:
@@ -147,7 +202,7 @@ class MainWindow(wx.Frame):
 
     def OnSave(self, event):
         textfile = open(os.path.join(self.dirname, self.filename), 'w')
-        textfile.write(self.control.GetValue())
+        textfile.write(self.console.GetValue())
         textfile.close()
 
     def OnOpen(self, event):
@@ -155,7 +210,7 @@ class MainWindow(wx.Frame):
             with open(os.path.join(self.dirname, self.filename), 'r') as file:
                 self.saga.load_database(file)
 
-            self.saga.game_loop(1)
+            self.saga.game_loop(2)
 
     def OnSaveAs(self, event):
         if self.askUserForFilename(defaultFile=self.filename, style=wx.SAVE,
@@ -166,10 +221,10 @@ if __name__ == '__main__':
     from pyscottfree import get_options
 
     (options, seed, args) = get_options(sys.argv)
-    app = wx.App()
-    frame = MainWindow(options, seed)
+    app = WxSaga(options, seed)
     if len(args) > 0:
         with open(args[0], 'r') as file:
-            frame.saga.load_database(file)
-    frame.Show()
-    app.MainLoop()
+            app.frame.saga.load_database(file)
+            app.frame.saga.game_loop(2)
+    app.frame.Show()
+    app.root.MainLoop()
